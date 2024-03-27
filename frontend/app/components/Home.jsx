f27d507cf825612ea6366c35ad49ddc3337adecd
harshau007 <amanupadhyay2004@gmail.com>
2024-03-27 18:49:39 +0530

    Tried to add backend

--- frontend/app/components/Home.jsx
+++ frontend/app/components/Home.jsx
@@ -1,6 +1,7 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import axios from "axios";
 
 const Home = () => {
     const [showModal, setShowModal] = useState(false);
@@ -92,40 +93,75 @@ const Home = () => {
 };
 
 const ImageNavbar = ({ imageName }) => {
-    const [status, setStatus] = useState("stopped");
+    const [status, setStatus] = useState([]);
+    const [isStarted, setIsStarted] = useState();
+    const [dockerData, setDockerData] = useState([])
+    const [id, setId] = useState([]);
+    const [img, setImg] = useState([]);
 
-    const handleStart = () => {
-        setStatus("started");
-        // Add logic to start the image
-        console.log("Image started");
+    const dockerFetch = async () => {
+        const res = await axios.get('http://localhost:3000/docker')
+        let test = [];
+        let testId = [];
+        let testStatus = [];
+        let testImg = [];
+        for (let i = 0; i < res.data.length; i++) {
+            let str = res.data[i].Names[0].slice(1)
+            let newId = res.data[i].Id.slice(0, 10);
+            let newStatus = res.data[i].State;
+            let newImg = res.data[i].Image;
+            test.push(str)
+            testId.push(newId);
+            testStatus.push(newStatus);
+            testImg.push(newImg);
+        }
+        setDockerData(res.data)
+        console.log(testImg)
+        setId(testId);
+        setStatus(testStatus)
+        setImg(testImg)
+    }
+
+    useEffect(() => {
+        dockerFetch();
+    }, [])
+
+    const handleStart = (id) => {
+        setIsStarted(true)
+        console.log(dockerData[id].State);
     };
 
-    const handleStop = () => {
-        setStatus("stopped");
+    const handleStop = (id) => {
         // Add logic to stop the image
-        console.log("Image stopped");
+        setIsStarted(false);
+        console.log(dockerData[id].State);
     };
 
     const handleDelete = () => {
         // Add logic to delete the image
-        console.log("Image deleted");
+        if (isStarted) {
+            console.log("Stop Container first");
+        } else {
+            console.log("Image deleted");
+        }
     };
     return (
         <div className="mt-10">
-            <div className="flex justify-between items-center w-full h-16 px-4 text-white bg-[#121212] rounded-lg border-t border-b border-green-500">
-                <div>
-                    <p className="text-lg">
-                        <span className="opacity-50">Image Name:  </span>
-                        <span>{imageName}</span>
-                    </p>
+            {dockerData.map((data, index) => {
+                <div className="flex justify-between items-center w-full h-16 px-4 text-white bg-[#121212] rounded-lg border border-green-500" key={index}>
+                    <div>
+                        <p className="text-lg">
+                            <span className="opacity-50">Image Name:  </span>
+                            <span>{img[index]}</span>
+                        </p>
+                    </div>
+                    <div>
+                        <button onClick={handleStart} className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full mr-2 disabled:opacity-50" disabled={isStarted}>Start</button>
+                        <button onClick={handleStop} className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full mr-2 disabled:opacity-50" disabled={!isStarted}>Stop</button>
+                        <button onClick={handleDelete} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full">Delete</button>
+                    </div>
                 </div>
-
-                <div>
-                    <button onClick={handleStart} className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full mr-2">Start</button>
-                    <button onClick={handleStop} className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full mr-2">Stop</button>
-                    <button onClick={handleDelete} className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full">Delete</button>
-                </div>
-            </div>
+            })}
         </div>
     );
 };
